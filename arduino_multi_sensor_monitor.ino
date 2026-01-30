a092186aef70d464afdd571a9d140013b09a6ed5
Gayathri5904 <gayathrivadapalli8@gmail.com>
2026-01-30 17:43:09 +0530

    Create arduino_multi_sensor_monitor.ino

---
+++ arduino_multi_sensor_monitor.ino
@@ -0,0 +1,36 @@
+// Pin definitions
+const int tempPin = A0;   // TMP36 / LM35
+const int ldrPin = A1;    // LDR
+const int ledPin = 8;     // LED
+const int buzzerPin = 9;  // Buzzer
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Multi-Sensor Monitoring System Started");
+}
+
+void loop() {
+  int tempValue = analogRead(tempPin);
+  int ldrValue = analogRead(ldrPin);
+
+  float voltage = tempValue * (5.0 / 1023.0);
+  float temperatureC = voltage * 100.0; // for TMP36/LM35
+
+  Serial.print("Temp: ");
+  Serial.print(temperatureC);
+  Serial.print(" C | LDR: ");
+  Serial.println(ldrValue);
+
+  if (temperatureC > 30 || ldrValue < 300) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzerPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(buzzerPin, LOW);
+  }
+
+  delay(1000);
+}
